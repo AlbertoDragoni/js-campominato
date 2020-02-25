aaa49f14b0ffc3c63f0047ae654dc97054d95866
Alberto Dragoni <albertodragoni@yahoo.it>
2020-02-25 15:55:02 +0100

    quasi

--- js/main.js
+++ js/main.js
@@ -6,26 +6,34 @@
 // massimo possibile di numeri consentiti.
 // // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte
 // che l’utente ha inserito un numero consentito.
-var numeriSceltiDalComputer = [];
+var mine = [];
 
 var i = 0;
 while (i < 16) {
     var numeriCasuali = generaRandom(1,100);
-    numeriSceltiDalComputer.push(numeriCasuali);
+    mine.push(numeriCasuali);
     i++;
 }
 function generaRandom(min, max) {
     numeroRandom = Math.floor(Math.random() * (max - min + 1)) + min;
     return numeroRandom;
 }
-console.log(numeriSceltiDalComputer);
+console.log(mine);
+// fino a qua ho generato 16 numeri random, le mine.
 
 var richiestaNumero = parseInt(prompt('inserisci un numero da 1 a 100'));
+
 var verificaNumero = false;
 
 if (!isNaN(richiestaNumero) && richiestaNumero < 100 && richiestaNumero > 1) {
     var verificaNumero = true;
 }
 if (verificaNumero){
-
+    if (mine.includes(richiestaNumero)) {
+        console.log('hai perso');
+    } else {
+        console.log('hai vinto');
+    }
+} else {
+    console.log('inserisci un numero da 1 a 100');
 }
